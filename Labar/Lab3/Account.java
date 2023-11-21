a491c35f8dbc728cc4fb6d3aeba017dca7636fa2
Lucas Baunsgaard <Lucas.baunsgaard@outlook.com>
2023-11-21 14:23:12 +0100

    Added more code

--- Labar/Lab3/Account.java
+++ Labar/Lab3/Account.java
@@ -1,8 +1,9 @@
 package lab3;
+import java.util.ArrayList;
 
 public class Account {
 
-	private static int accountNumbers = 1;
+	private static int accountNumber = 1;
 	private String customer;
 	private int thisAccountNumber;
 	private double balance;
@@ -18,12 +19,47 @@ public class Account {
 		accountNumber += 1;
 	}
 	
-	public Account(String arg1, double agr2, double arg3) {
+	public Account(String arg1, double arg2, double arg3) {
 		customer = arg1;
-		balance = arg2;
+		balance = arg3;
 		
-		
+		//Lite mer saker ska in här men fattar ej vad.
+	}
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public String getCustomer() {
+		return customer;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(double arg1) {
+		balance = arg1;
+	}
+	
+	public static void setBank(Bank arg1) {
+		theBank = arg1;
+	}
+	
+	public SavingAccount getSavingsAccount() {
+		if(true) {
+			/*
+			getSavingsAccount() returns otherAccount if that is a savings account. 
+			It returns null if otherAccount is a current account.
+
+			 */
+		}
+	} 
+	
+	public String toString() {
+		String output = "";
 		
 	}
+		
 	
 }
