c69cada70cf092ec6283ad0914c037a6c5b06b84
Lucas Baunsgaard <Lucas.baunsgaard@outlook.com>
2023-11-21 18:30:27 +0100

    update

--- Labar/Lab3/CurrentAccount.java
+++ Labar/Lab3/CurrentAccount.java
@@ -10,10 +10,25 @@ public class CurrentAccount extends Account {
 	}
 	
 	public CurrentAccount(String arg1, double arg2, double arg3) {
+		super (arg1, arg2, arg3);
 		
 	}
 	
 	public void savings(double arg1) {
+		if(!this.getSavingsAccount().equals(null)) {
+			if(arg1 > 0) {
+				if(arg1 <= this.getBalance()) {
+					double temp = this.getBalance() - arg1;
+					this.setBalance(temp);
+					
+					
+				}
+			}
+			if(arg1 <= 0 ) {
+
+				//text
+			}
+		}
 		
 	}
 }
