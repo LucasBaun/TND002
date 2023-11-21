2722452f6e3a5dc7088696a49781c8685f3d9a81
Lucas Baunsgaard <Lucas.baunsgaard@outlook.com>
2023-11-21 14:03:18 +0100

    Added some more stuff

--- Labar/Lab3/Account.java
+++ Labar/Lab3/Account.java
@@ -6,6 +6,24 @@ public class Account {
 	private String customer;
 	private int thisAccountNumber;
 	private double balance;
+	protected static Bank theBank;
+	protected Account otherAccount;
+	protected ArrayList<String> transactions = new ArrayList<String>();
+
+	public Account(String arg1, double arg2) {
+		customer = arg1;
+		balance = arg2;
+		
+		thisAccountNumber = accountNumber;
+		accountNumber += 1;
+	}
 	
+	public Account(String arg1, double agr2, double arg3) {
+		customer = arg1;
+		balance = arg2;
+		
+		
+		
+	}
 	
 }
