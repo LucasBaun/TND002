7ff1aa44efb12632da39963628d63f42616c2588
Lennetorp <kunglinus02@hotmail.se>
2023-12-15 12:47:29 +0100

    Lab6 lite kommentarer

--- Labar/lab6/GUI.java
+++ Labar/lab6/GUI.java
@@ -10,37 +10,51 @@ import javax.swing.*;
 
 
 public class GUI extends JFrame implements ActionListener {
-	private PhoneBook telefonLista;
 	
-	private JButton lPhonebook = new JButton("Load phonebook");		
-	private JButton sPhonebook = new JButton("Save phonebook");
-	private JButton search = new JButton("Search");
-	private JButton nextName = new JButton("Next name");
-	private JButton addPerson = new JButton("Add Person");
-	private JButton deletePerson = new JButton("Delete person");
+	private JButton lPhonebook; //Buttons
+	private JButton sPhonebook;
+	private JButton search;
+	private JButton nextName;
+	private JButton addPerson;
+	private JButton deletePerson;
 	
-	private JTextField top = new JTextField();
-	private JTextField middle = new JTextField();
-	private JTextField bottom = new JTextField();
+	private JTextField top; //TextField
+	private JTextField middle;
+	private JTextField bottom;
+	
+	private PhoneBook telefonLista = new PhoneBook();  //The loaded phone book
+	ArrayList<Person> lucas = new ArrayList<Person>(); //Our temporary search array when using nextName button
+	private int counter = 1; //Counter that keeps track of how many times we pressed nextName button
+	boolean doublePress = true; //A counter but in boolean type, for the addPerson method
 	
 	public GUI() {
-		telefonLista = new PhoneBook();
-		Font theFont = new Font("SansSerif", Font.PLAIN, 20);
-//		setSize(800,400);
-		setVisible(true);		
+		
+		//Misc
+		Font theFont = new Font("SansSerif", Font.PLAIN, 20); //Sets font
+		setVisible(true); 		
 		setTitle("Interactive phonebook");
-		Container c = getContentPane();
-		c.setLayout(new GridLayout(3,2));
+		Container c = getContentPane(); //Create a container where we add all of our buttons and textfields
+		c.setLayout(new GridLayout(3,2)); //In math 3 rows, 2 colons
+		
+		//Initialize
+		top = new JTextField();
+		middle = new JTextField();
+		bottom = new JTextField();
+		lPhonebook = new JButton("Load phonebook");		
+		sPhonebook = new JButton("Save phonebook");
+		search = new JButton("Search");
+		nextName = new JButton("Next name");
+		addPerson = new JButton("Add Person");
+		deletePerson = new JButton("Delete person");
 
-		top.setFont(theFont);
-		middle.setFont(theFont);
-		bottom.setFont(theFont);
-		middle.setEnabled(false);
-		bottom.setEnabled(false);
-		top.addActionListener(this);
-		middle.addActionListener(this);
-		bottom.addActionListener(this);
-
+		// Set so middle and bottom button not editable
+		middle.setEditable(false);
+		bottom.setEditable(false);
+		
+		//setEnabled or not
+		top.setEnabled(true);
+		middle.setEnabled(true);		
+		bottom.setEnabled(true);
 		lPhonebook.setEnabled(true);
 		sPhonebook.setEnabled(false);
 		search.setEnabled(false);
@@ -48,6 +62,10 @@ public class GUI extends JFrame implements ActionListener {
 		addPerson.setEnabled(false);
 		deletePerson.setEnabled(false);
 		
+		//Set the font
+		top.setFont(theFont);
+		middle.setFont(theFont);
+		bottom.setFont(theFont);
 		lPhonebook.setFont(theFont);
 		sPhonebook.setFont(theFont);
 		search.setFont(theFont);
@@ -55,15 +73,18 @@ public class GUI extends JFrame implements ActionListener {
 		addPerson.setFont(theFont);
 		deletePerson.setFont(theFont);
 		
+		//Set to the ActionListener reacts when button is pressed
+		top.addActionListener(this);
+		middle.addActionListener(this);
+		bottom.addActionListener(this);
 		lPhonebook.addActionListener(this);
 		sPhonebook.addActionListener(this);
 		search.addActionListener(this);
 		nextName.addActionListener(this);
 		addPerson.addActionListener(this);
-		deletePerson.addActionListener(this);
+		deletePerson.addActionListener(this);		
 		
-		
-		
+		//Add the elements
 		c.add(lPhonebook);
 		c.add(sPhonebook);
 		c.add(top);
@@ -74,28 +95,27 @@ public class GUI extends JFrame implements ActionListener {
 		c.add(deletePerson);
 		c.add(bottom);
 		
-
+		// Pack everything together
 		pack();		
+		//Stop run code when exiting GUI
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
 
 	
 	public void actionPerformed(ActionEvent ae) {
-		
-		String temp = "";
-		
-		ArrayList<Person> lucas = new ArrayList<Person>();
-		if (ae.getSource() == lPhonebook || ae.getSource() == top) {
-				try {
-					temp = telefonLista.load(top.getText());
-					middle.setText(temp);
-				
-				} catch (IOException e) {
-					System.out.println("Failed to load");
-			}
-			
-			if (temp.equals("Phonebook loaded")) {
+		String temp = "";		
+
+		if (ae.getSource() == lPhonebook || ae.getSource() == top) { //When Pressing LoadButton or enter
+			doublePress = false; //Reset addPerson button				
+			temp = telefonLista.load(top.getText()); //loads file, contains try catch 
+			middle.setText(temp);
+				if (temp.equals("Try Again")) {
+					sPhonebook.setEnabled(false);
+					search.setEnabled(false);
+					addPerson.setEnabled(false);
+					deletePerson.setEnabled(false);	
+				} else if (temp.equals("Phonebook loaded")) { //If loaded enable the other buttons
 				top.setText("");
 				bottom.setText("");
 				sPhonebook.setEnabled(true);
@@ -105,56 +125,103 @@ public class GUI extends JFrame implements ActionListener {
 			}		
 			
 		}
-		if (ae.getSource() == sPhonebook) {
-			temp = "";
+		if (ae.getSource() == sPhonebook) { //When pressing Save phone book
+			doublePress = false; //Reset addPerson booleon
 			temp = top.getText();			
 			
-			if (temp.equals("")) {
+			if (temp.isBlank() || temp.equals("PhoneList.txt")) { 
 				middle.setText("Provide file namn");
 				bottom.setText("");
 			} else {
-				try {
-					temp = telefonLista.save(temp);
-					middle.setText(temp);
-				} catch (IOException e) {
-					System.out.println("Failed");
-				}
-			}
-			
+				temp = telefonLista.save(temp); //Contains try catch
+				middle.setText(temp);	
+			}			
 		}
-		if (ae.getSource() == search) {
-			System.out.println("Works");
+		
+		if (ae.getSource() == search) {	//When pressing Search button
+			doublePress = false; //Reset		
+			lucas.clear(); //Reset
+			counter = 1; //Reset
 			
-			temp = "";
 			temp = top.getText();
-			
-			lucas = telefonLista.search(temp);
+			lucas = telefonLista.search(temp); //Searches for person and return array
 			if (lucas.isEmpty()) {
-				bottom.setText("Provide a name");
+				bottom.setText("Provide a name or number");
+				middle.setText("");
+			} else if (lucas.size() == 1) { //If search method only found one display and don't turn on nextName
+					middle.setText(lucas.get(0).getFullName());
+					bottom.setText(Integer.toString(lucas.get(0).getPhoneNumber()));
+			}
+			else if (lucas.size() > 1) { //If found more than one enable nextName
+				nextName.setEnabled(true);
+				middle.setText(lucas.get(0).getFullName());
+				bottom.setText(Integer.toString(lucas.get(0).getPhoneNumber()));
+			}		
+		}	
+		if (ae.getSource() == nextName) { //When pressing nextName
+			if (counter >= lucas.size() || lucas.size() == 1) { //If Counter is bigger than the persons found
+				nextName.setEnabled(false); //then disable button otherwise array out of bounds exception
+				counter = 1;
+				
 			} else {
-				if (lucas.size() == 1) {
-					middle.setText(lucas.get(0).getFullName() + " " + lucas.get(0).getPhoneNumber());
-				}
-				else if (lucas.size() > 1) {
-					nextName.setEnabled(true);
-					nextName.doClick();
-					
+				middle.setText(lucas.get(counter).getFullName());
+				bottom.setText(Integer.toString(lucas.get(counter).getPhoneNumber()));
+				counter += 1;
+				if (counter >= lucas.size()) { //Then check again if counter bigger than array then disable button
+					nextName.setEnabled(false);
 				}
 			}	
-			top.setText("");
-
 		}
-//		int counter = 0;
-//		if (ae.getSource() == nextName) {
-//			
-//			
-//			middle.setText(lucas.get(counter).getFullName());
-//			bottom.setText(Integer.toString(lucas.get(counter).getPhoneNumber()));
-//			counter =+ 1;
-//			System.out.println("counter increase");
-//			
-//		}
+		if (ae.getSource() == deletePerson) {
+			doublePress = false;
+			try {
+				top.setText(telefonLista.deletePerson(middle.getText(), Integer.parseInt(bottom.getText())));
+				middle.setText("");
+				bottom.setText("");
+				if (lucas.size() == 1) {
+					nextName.setEnabled(false);
+				}
+			}catch (Exception e) {
+				top.setText("Can't find person");
+			}
+			temp = middle.getText() + " " + bottom.getText();
+			
+		}
+		
+		if (ae.getSource() == addPerson) {
+			if (!doublePress) {
+				top.setText("Type in name and number");
+				middle.setText("");
+				middle.setEditable(true);
+				bottom.setText("");
+				bottom.setEditable(true);				
+				doublePress = true;				
+			} else {
+				try {
+					boolean answer = telefonLista.addPerson(middle.getText(), Integer.parseInt(bottom.getText()));
+					middle.setText("");
+					bottom.setText("");
+					middle.setEditable(false);					
+					bottom.setEditable(false);
+					doublePress = false;
+					if (answer) {
+						top.setText("Person added");
+					} else {
+						top.setText("Person could not be added");
+					}					
+				} catch (Exception e) {
+					top.setText("Something went wrong");
+					middle.setEditable(false);					
+					bottom.setEditable(false);
+				}
+			}
+		}		
+	}
+
+	public static void main(String[] args) throws IOException{
+		GUI g = new GUI();		
 
 	}
-	
 }
+	
+
--- Labar/lab6/PhoneBook.java
+++ Labar/lab6/PhoneBook.java
@@ -18,52 +18,54 @@ public class PhoneBook {
 //		return temp;
 //	}
 	
-	public String load(String arg) throws IOException{
+	public String load(String arg) { //Load in File
+		listOfNumbers.clear(); //Reset when using GUI load button multiple times
+		//Puts arg in to File then tests if File(arg) can read the file
 		File myFile = new File(arg);
 		if (!myFile.canRead()) {
 			return "Try Again";
 		}
-		FileReader fRead = new FileReader(myFile);		
-		BufferedReader bread = new BufferedReader(fRead);
-		String temp = "";		
-		String[] statArray;
-		while ((temp = bread.readLine()) != null) {
-			statArray = temp.split("\\s");
-			if (statArray.length == 3) {
-				try {				
-					listOfNumbers.add(new Person(statArray[0], statArray[1], Integer.parseInt(statArray[2])));
-//					System.out.println(statArray[0] + statArray[1] + Integer.parseInt(statArray[2]));
-				
-				} catch (NumberFormatException ierr) {
-					bread.close();
-					return "Try Again : " + ierr.getMessage();
+		String temp = "";				
+		try { //Here we create FileReader which reads in ascii and therefor puts i BufferedReader to translate
+			String[] statArray = {};
+			FileReader fRead = new FileReader(myFile);		
+			BufferedReader bread = new BufferedReader(fRead);
+			while ((temp = bread.readLine()) != null) { //Bread reads every line and splits in to three elements
+				statArray = temp.split("\\s");
+				if (statArray.length == 3) { //Add the three element in to dynamic array, but need try
+					// catch because we use Integer.parseInt to convert String to int
+					listOfNumbers.add(new Person(statArray[0], statArray[1], Integer.parseInt(statArray[2])));//							
 				}
 			}
-
-			
-		}
-
-		bread.close();
+			bread.close(); //Close the Reader
+			} catch (Exception ierr) {					
+				return "Try Again";
+			}		
 		return "Phonebook loaded";	
 	}
 	
-	public ArrayList<Person> search(String arg) {
+	public ArrayList<Person> search(String arg) { //Search for person in array
 		ArrayList<Person> temp = new ArrayList<Person>();
 		for (Person check : listOfNumbers) {		
 			if (check.getSurname().equals(arg) || Integer.toString(check.getPhoneNumber()).equals(arg)) {
-				temp.add(check);
+				temp.add(check); //If found person add it to an temporary array, if found multiple add also to array
 			}
 		}
 		return temp;
 	}
 	
-	public String deletePerson(String arg1, int arg2) {
+	public String deletePerson(String arg1, int arg2) { //Deletes person from dynamic array listOfNumbers
 		ArrayList<Person> temp = new ArrayList<Person>();
-		temp = search(Integer.toString(arg2));
+		try { // Try-catch int to string
+		temp = search(Integer.toString(arg2)); //Search for the person(s)
+		} catch (Exception e) {
+			return e.getMessage();
+		}
 		for (Person same : temp) {
-			if (arg1.equals(same.getFullName())) {
+			if (arg1.equals(same.getFullName())) { // if arg1 full name equals the search arrays full name
 				for (int pass = 0; pass < listOfNumbers.size(); ++pass) {
-					if (listOfNumbers.get(pass).equals(same)) {
+					if (listOfNumbers.get(pass).equals(same)) { //then goes through the listOfNumbers and deletes
+						//if it finds
 						listOfNumbers.remove(pass);
 						return "Person deleted";
 					}
@@ -78,14 +80,17 @@ public class PhoneBook {
 	public boolean addPerson(String arg1, int arg2) {		
 		ArrayList<Person> temp = new ArrayList<Person>();
 		String[] name;
-		temp = search(Integer.toString(arg2));
-		
-		if (temp.isEmpty()) {
-			name = arg1.split(" +");
-			if(name.length == 2) {
+		try {
+			temp = search(Integer.toString(arg2));
+		} catch (Exception e) {
+			return false;
+		}		
+		if (temp.isEmpty()) { //If search method did not find, it will return empty array, then we can add person
+			name = arg1.split(" +"); //Split at spaces
+			if(name.length == 2) { //Name has to be a first name and a surname, not more or less
 				for (Person check : temp) {
-					if (check.getPhoneNumber() == arg2) {
-						
+					if (check.getPhoneNumber() == arg2) { //if persons number already exist don't add
+						return false;
 					}
 				}
 				listOfNumbers.add(new Person(name[0], name[1], arg2));
@@ -95,19 +100,18 @@ public class PhoneBook {
 		return false;		
 	}
 	
-	public String save(String arg) throws IOException{
-		BufferedWriter Wread = new BufferedWriter(new FileWriter(arg));
+	public String save(String arg) {		
 		String output = "";
-		for (int pass = 0; pass < listOfNumbers.size(); ++pass) {
-			output += listOfNumbers.get(pass).getFullName() + " " +listOfNumbers.get(pass).getPhoneNumber() + "\n";
-		}
 		try {
-			Wread.write(output);
-		} catch (IOException e) {
-			Wread.close();
+			BufferedWriter Wread = new BufferedWriter(new FileWriter(arg)); //Create BufferedWriter which writes to file
+			for (int pass = 0; pass < listOfNumbers.size(); ++pass) { //Goes through array and adds to string
+				output += listOfNumbers.get(pass).getFullName() + " " +listOfNumbers.get(pass).getPhoneNumber() + "\n";
+			}
+			Wread.write(output); //Writes
+			Wread.close(); //Flush or closes
+		} catch (IOException e) {			
 			return "Could not save to the file";
-		}
-		Wread.close();
+		}		
 		return "Saved " + listOfNumbers.size() + " people to the file.";
 	}
 	
